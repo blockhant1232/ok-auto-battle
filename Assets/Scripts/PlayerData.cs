a9c2f5f65937f0e01329bf92038688a12a157d7e
blockhant1232 <blockhant1232@gmail.com>
2022-08-04 15:44:21 +0700

    Add empty project

--- Assets/Scripts/PlayerData.cs
+++ Assets/Scripts/PlayerData.cs
@@ -1,16 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerData : Manager<PlayerData>
 {
     public int Money { get; private set; }
 
     public System.Action OnUpdate;
+    public Text text;
 
     private void Start()
     {
         Money = 250;
+        text.text = Money.ToString();
     }
 
     public bool CanAfford(int amount)
@@ -22,5 +25,10 @@ public class PlayerData : Manager<PlayerData>
     {
         Money -= amount;
         OnUpdate?.Invoke();
+    }
+
+    public void Update()
+    {
+        text.text = Money.ToString();
     }
 }
